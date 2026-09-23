4995239eb28440ca83b28dff63cdaa9fe103ef8a
mokes <mokes@otherMynd.com>
2026-09-23 14:58:07 +0100

    chore: v0.4.2

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -32,8 +32,8 @@ android {
         applicationId = "dev.swarmz.phone"
         minSdk = 31
         targetSdk = 35
-        versionCode = 401
-        versionName = "0.4.1"
+        versionCode = 402
+        versionName = "0.4.2"
     }
 
     signingConfigs {
